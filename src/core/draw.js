abc7d2633fff183adfb1354c28c99040b1c3f35c
Luca Picci <lpicci96@gmail.com>
2026-02-12 20:35:02 +0100

    Add SVG size adjustment functionality

--- src/core/draw.js
+++ src/core/draw.js
@@ -1,9 +1,25 @@
 import data from "./data";
 import { layout } from "../init";
 
-export default function() {
+let svg;
 
-    const container = layout.getSection("primary");
-    // TODO: Placeholder to append SVG
-
+export function sizeSvg() {
+    const width = layout.getPrimaryWidth();
+    const height = layout.getPrimaryHeight();
+    svg.setAttribute("width", width);
+    svg.setAttribute("height", height);
+    svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
 }
+
+export default function() {
+    const container = layout.getSection("primary");
+
+    layout.update();
+
+    svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.style.border = "1px solid red";
+    container.appendChild(svg);
+    sizeSvg();
+}
+
+export { svg };
--- src/core/update.js
+++ src/core/update.js
@@ -1,7 +1,8 @@
 import data from "./data";
 import { layout } from "../init";
+import { sizeSvg } from "./draw";
 
 export default function() {
-
     layout.update();
+    sizeSvg();
 }
