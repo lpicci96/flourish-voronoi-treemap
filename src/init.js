092b8cf638803668624777c7da6f87d2dbc1cf29
Luca Picci <lpicci96@gmail.com>
2026-02-13 10:23:57 +0100

    Add legend container and categorical legend initialization in init.js

--- src/init.js
+++ src/init.js
@@ -1,6 +1,9 @@
 import initLayout from "@flourish/layout";
 import createColors from "@flourish/colors";
+import { createLegendContainer, createDiscreteColorLegend } from "@flourish/legend";
 import state from "./core/state";
 
 export var layout = initLayout(state.layout);
 export var colors = createColors(state.colors);
+export var legend_container = createLegendContainer(state.legend_container);
+export var legend_categorical = createDiscreteColorLegend(state.legend_categorical);
