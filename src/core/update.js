c8dfd130c3422a45dfbdcc47c88d01cc897c2aa6
Luca Picci <lpicci96@gmail.com>
2026-02-13 10:23:43 +0100

    Update legend handling in update.js to allocate space before layout

--- src/core/update.js
+++ src/core/update.js
@@ -1,15 +1,21 @@
 import data from "./data";
 import state from "./state";
-import { layout, colors } from "../init";
-import { sizeSvg, svg } from "./draw";
+import { layout, colors, legend_container, legend_categorical } from "../init";
+import { sizeSvg, svg, updateLegend } from "./draw";
 import { processData, drawVoronoi } from "./chart/voronoi";
 
 export default function() {
+    const hierarchy = processData(data);
+    if (!hierarchy) {
+        layout.update();
+        sizeSvg();
+        return;
+    }
+
+    // Update legend before layout so layout allocates space for it
+    updateLegend(hierarchy);
     layout.update();
     sizeSvg();
-
-    const hierarchy = processData(data);
-    if (!hierarchy) return;
 
     const width = layout.getPrimaryWidth();
     const height = layout.getPrimaryHeight();
